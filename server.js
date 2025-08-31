84cb0c43c6af980481aaeceabc82789794b3fdb9
t-fb <t.fabrigar@gmail.com>
2025-08-30 19:34:15 -0700

    ideas on schema and routes

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const { Client } = require("pg");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-async function startServer() {
-  const pgClient = new Client({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    password: process.env.PASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
-  });
+const PORT = 3000;
 
+const pgClient = new Client({
+  user: "postgres",
+  host: "localhost",
+  password: "password",
+  database: "requestbin",
+  port: 5432,
+});
+
+async function startServer() {
   try {
     await pgClient.connect();
     console.log("Connected to PostgreSQL");
@@ -29,3 +32,53 @@ async function startServer() {
 }
 
 startServer();
+
+app.use(express.json());
+
+// This route will handle all HTTP methods for the basket_path
+app.all("/:path", async (req, res) => {
+  const method = req.method;
+  const path = req.path;
+  const header = req.headers;
+  const body = req.body; // Use for POST
+
+  // Pretend a GET just came in
+  // Query Postgres to find path id
+  const result = await pgClient.query("SELECT id FROM basket WHERE path=$1", [
+    path.replace("/", ""),
+  ]);
+
+  //TODO: add if statement to handle case where there is no such request bin
+  const basketId = result.rows[0].id;
+
+  // Find body ref in mongoDB
+  const mongodbPath = "pretend this is a ref to mongoDB";
+
+  pgClient.query(
+    "INSERT INTO request (basket_endpoint_id, method, header,  mongodb_path) values ($1, $2, $3, $4)",
+    [basketId, method, header, mongodbPath]
+  );
+
+  res.send({});
+});
+
+// Create basket
+app.post("/api/basket/:key", (req, res) => {
+  console.log(`you are creating basket: ${req.params.key}`);
+
+  const path = req.params.key;
+
+  pgClient.query("INSERT INTO basket (endpoint_id) VALUES ($1)", [path]);
+
+  res.send({});
+});
+
+app.get("/api/basket", (req, res) => {
+  // TODO: I need to generate the url with key
+  console.log("you requested a basket");
+  res.send({});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
--- setup.sql
+++ setup.sql
@@ -3,13 +3,14 @@ CREATE DATABASE requestbin;
 \c requestbin
 
 CREATE TABLE basket (
-  endpoint_id text PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
+  path text NOT NULL, 
   total_request INTEGER DEFAULT 0
 );
 
 CREATE TABLE request (
   id SERIAL PRIMARY KEY,
-  basket_endpoint_id text NOT NULL REFERENCES basket (endpoint_id) ON DELETE CASCADE,
+  basket_endpoint_id integer NOT NULL REFERENCES basket (id) ON DELETE CASCADE,
   method text NOT NULL,
   header text NOT NULL,
   time_stamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
