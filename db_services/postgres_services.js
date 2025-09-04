4a194a57b3862977f1a5443437212a131f657da0
ZBajcar <bajcarzach@icloud.com>
2025-09-03 21:47:35 -0400

    added error handling for psql services

--- db_services/postgres_services.js
+++ db_services/postgres_services.js
@@ -1,67 +1,83 @@
 const { Client } = require("pg");
+
 const pgClient = new Client({
   connectionString: process.env.REQUESTBIN_POSTGRES_URL,
 });
 
-async function connectPostgres() {
+async function connectToPostgres() {
   try {
     await pgClient.connect();
-    console.log("Connected to PostgreSQL");
+    console.log("Successfully connected to PostgreSQL");
   } catch (err) {
-    console.error("PostgreSQL connection error:", err);
-    process.exit(1);
+    console.error("Error connecting to PostgreSQL:");
+    throw err;
   }
 }
-connectPostgres();
 
 // get all baskets
 async function getAllBaskets() {
-  // query all baskets from `basket` table
-  const result = await pgClient.query("SELECT * FROM basket");
-  return result.rows;
+  try {
+    // query all baskets from `basket` table
+    const result = await pgClient.query("SELECT * FROM basket");
+    return result.rows;
+  } catch (err) {
+    console.error('Error getting all baskets');
+    throw err;
+  }
+  
 }
 
 // create new basket
 async function createBasket(path) {
-  // make query to basket where path = req.params.key
-  // RETRURN * means return newly inserted row
-  const result = await pgClient.query(
-    "INSERT INTO basket (path_name) VALUES ($1) RETURNING *",
-    [path]
-  );
-  return result.rows[0];
+  try {
+    // make query to basket where path = req.params.key
+    // RETRURN * means return newly inserted row
+    const result = await pgClient.query("INSERT INTO basket (path_name) VALUES ($1) RETURNING *", [path]);
+    return result.rows[0];
+  } catch (err) {
+    console.error("Error creating a new basket");
+    throw err;
+  }
 }
 
 // list all requests from basket
 async function getRequestsForBasket(basketPathName) {
-  // query rows from `request` where `basket_enpoint_id` = `basketPathName`
-  const result = await pgClient.query(
-    "SELECT * FROM request WHERE basket_path_name = $1",
-    [basketPathName]
-  );
-  return result.rows;
+  try {
+    // query rows from `request` where `basket_enpoint_id` = `basketPathName`
+    const result = await pgClient.query("SELECT * FROM request WHERE basket_path_name = $1", [basketPathName]);
+    console.log(result.rows)
+    return result.rows;
+  } catch (err) {
+    console.error("Error getting all requests for basket");
+    throw err;
+  }
 }
 
 // delete basket
 async function deleteBasket(path) {
-  // delete row from `basket` table where path = req.params.key
-  const result = await pgClient.query(
-    "DELETE FROM basket WHERE path_name = $1",
-    [path]
-  );
-  return result.rowCount;
+  try {
+    // delete row from `basket` table where path = req.params.key
+    const result = await pgClient.query("DELETE FROM basket WHERE path_name = $1", [path]);
+    return result.rowCount;
+  } catch (err) {
+    console.error("Error deleting basket");
+    throw err;
+  }
 }
 //  add webhook requests to specified basket
 async function addRequest(basketPathName, method, header, mongodbPath) {
-  const result = await pgClient.query(
-    "INSERT INTO request (basket_path_name, method, header, mongodb_path) VALUES ($1, $2, $3, $4)",
-    [basketPathName, method, header, mongodbPath]
-  );
-
-  return result.rows[0];
+  try {
+    //add request to basket
+    const result = await pgClient.query("INSERT INTO request (basket_path_name, method, header, mongodb_path) VALUES ($1, $2, $3, $4)", [basketPathName, method, header, mongodbPath]);
+    return result.rows[0];
+  } catch (err) {
+    console.error("Error adding request to basket");
+    throw err;
+  }
 }
 
 module.exports = {
+  connectToPostgres,
   getAllBaskets,
   createBasket,
   getRequestsForBasket,
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const {
-  getAllBaskets,
-  createBasket,
-  getRequestsForBasket,
-  deleteBasket,
-  addRequest,
-} = require("./db_services/postgres_services");
+const psqlServices = require("./db_services/postgres_services");
 const mongoServices = require("./db_services/mongo_services");
 
 const PORT = 3000;
@@ -16,9 +10,11 @@ async function startServer() {
 
   try {
     await mongoServices.connectToMongoDB();
-    console.log("Connected to MongoDB");
+    await psqlServices.connectToPostgres()
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.error("Failed to start server due to a database connection error.");
+    console.error(err);
+    process.exit(1);
   }
 }
 
@@ -28,7 +24,7 @@ app.use(express.json());
 
 // View all baskets
 app.get("/api/basket", async (req, res) => {
-  const result = await getAllBaskets();
+  const result = await psqlServices.getAllBaskets();
   res.send(result);
 });
 
@@ -38,7 +34,7 @@ app.post("/api/basket/:key", async (req, res) => {
   console.log(`you are creating basket: ${req.params.key}`);
 
   const path = req.params.key;
-  const result = await createBasket(path);
+  const result = await psqlServices.createBasket(path);
   // sends back newly created basket
   res.send(result);
 });
@@ -46,8 +42,9 @@ app.post("/api/basket/:key", async (req, res) => {
 // View a basket
 app.get("/api/basket/:key", async (req, res) => {
   const path = req.params.key;
-  const basketRequests = await getRequestsForBasket(path);
-
+  const basketRequests = await psqlServices.getRequestsForBasket(path);
+  //basket request is an empty array if that basket is deleted and we are not getting an error or anything when we try to view a deleted basket
+  //check this tomorrow 
   // return result
   res.send(basketRequests);
 });
@@ -55,7 +52,7 @@ app.get("/api/basket/:key", async (req, res) => {
 // Delete a basket
 app.delete("/api/basket/:key", async (req, res) => {
   const path = req.params.key;
-  const result = await deleteBasket(path); // returns row count of deleted rows
+  const result = await psqlServices.deleteBasket(path); // returns row count of deleted rows
 
   // if rowCount is more than 1 , row was deleted
   if (result > 0) {
@@ -80,7 +77,7 @@ app.all("/:path", async (req, res) => {
     
     // Then store the request metadata in PostgreSQL with the MongoDB reference
     console.log(savedRequestBody._id)
-    const request = await addRequest(path, method, headers, savedRequestBody._id.toString());
+    const request = await psqlServices.addRequest(path, method, headers, savedRequestBody._id.toString());
     
     // let test = await mongoServices.getRequestBody("68b764f1d9de2d9ad57bf964");
     // console.log(test);
